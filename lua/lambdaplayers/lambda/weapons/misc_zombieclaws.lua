98bd4c8e4d7d454aaa21f9bfcf28d4e63a731c26
FluffiestFloof <thebluewulf@gmail.com>
2022-11-06 14:59:09 +0100

    Zombie claws almost complete

--- lua/lambdaplayers/lambda/weapons/misc_zombieclaws.lua
+++ lua/lambdaplayers/lambda/weapons/misc_zombieclaws.lua
@@ -1,7 +1,6 @@
 local random = math.random
 table.Merge( _LAMBDAPLAYERSWEAPONS, {
--- Missing a lot from the Zeta version
--- Missing leap attack, regen overtime, hp on kill
+-- Missing leap attack
 
     zombieclaws = {
         model = "models/hunter/plates/plate.mdl",
@@ -13,18 +12,29 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
         keepdistance = 30,
         attackrange = 65,
         addspeed = 100,
+        
         OnEquip = function( lambda, wepent )
-            --I'm sure this is fine will not cause a single issue :)
+            local NextHPRegenTime = CurTime() + 0.5
+            
+            -- Damage reduction
             lambda:Hook( "EntityTakeDamage", "ZombieClawsETD", function( target, dmginfo )
                 if target == lambda then
                     dmginfo:ScaleDamage( 0.75 )
                 end
             end)
+            
+            -- HP Auto Regen
+            lambda:Hook( "Think", "ZombieClawsThink", function( )
+                if NextHPRegenTime and CurTime() > NextHPRegenTime and lambda:Health() < lambda:GetMaxHealth() then
+                    lambda:SetHealth(math.min(lambda:Health() + 1, lambda:GetMaxHealth()))
+                    NextHPRegenTime = CurTime() + 0.5
+                end
+            end)
         end,
         
         OnUnequip = function( lambda, wepent )
-            --Can't test it but I hope it works
             lambda:RemoveHook( "EntityTakeDamage", "ZombieClawsETD" )
+            lambda:RemoveHook( "Think", "ZombieClawsThink" )
         end,
         
         callback = function( self, wepent, target )
@@ -36,7 +46,8 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
             
             -- To make sure damage syncs with the animation
             timer.Simple(0.75, function()
-                if !IsValid(self) or self:GetIsDead() or !IsValid(target) or self:GetRangeTo(target) > (65) then return end
+                if !IsValid(self) or self:GetIsDead() or !IsValid(target) then return end
+                if self:GetRangeTo(target) > (65) then wepent:EmitSound("npc/zombie/claw_miss"..random(2)..".wav", 70) return end
                 
                 local dmg = random(35,55)
                 local dmginfo = DamageInfo()
@@ -47,13 +58,11 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
                 dmginfo:SetDamageForce( ( target:WorldSpaceCenter() - self:WorldSpaceCenter() ):GetNormalized() * dmg )
                 
                 target:EmitSound("npc/zombie/claw_strike"..random(3)..".wav", 70)
+                
                 -- HP regen on attacks
-                -- check if target:Alive because currently lambda just keep attacking the player entity
-                if self:Health() < self:GetMaxHealth() * 2.25 then
+                if self:Health() < self:GetMaxHealth() * 2.25 and target:Alive() then
                     self:SetHealth(math.min(self:Health() + self:GetMaxHealth() * math.Rand(0.10, 0.25), self:GetMaxHealth() * 2.25))
                 end
-                
-                
                 
                 target:TakeDamageInfo( dmginfo )
             end)
