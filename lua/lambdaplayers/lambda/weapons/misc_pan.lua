69e8f9c0c85618659e271f41a90058e45ca1e857
Floofers <thebluewulf@gmail.com>
2022-11-08 11:14:22 +0100

    Sound fix

--- lua/lambdaplayers/lambda/weapons/misc_pan.lua
+++ lua/lambdaplayers/lambda/weapons/misc_pan.lua
@@ -1,6 +1,5 @@
 local random = math.random
 local CurTime = CurTime
-local Rand = math.Rand
 
 table.Merge( _LAMBDAPLAYERSWEAPONS, {
 
@@ -17,7 +16,7 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
         callback = function( self, wepent, target )
             self.l_WeaponUseCooldown = CurTime() + 0.5
 
-            wepent:EmitSound( "lambdaplayers/pan/pan_miss.wav", 70, 100, 1, CHAN_WEAPON )
+            wepent:EmitSound( "lambdaplayers/pan/melee_pan_miss1.wav", 70, random(98,102), 1, CHAN_WEAPON )
             self:RemoveGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_MELEE )
             self:AddGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_MELEE )
             
@@ -25,17 +24,16 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
             self:SimpleTimer(0.25, function()
                 if self:GetRangeSquaredTo(target) > (70 * 70) then return end
                 
-                local dmg = 15
-                local dmginfo = DamageInfo()
-                dmginfo:SetDamage(dmg)
-                dmginfo:SetAttacker(self)
-                dmginfo:SetInflictor(wepent)
-                dmginfo:SetDamageType(DMG_CLUB)
-                dmginfo:SetDamageForce( ( target:WorldSpaceCenter() - self:WorldSpaceCenter() ):GetNormalized() * dmg )
+                local dmg = DamageInfo()
+                dmg:SetDamage( 15 )
+                dmg:SetAttacker( self )
+                dmg:SetInflictor( wepent )
+                dmg:SetDamageType( DMG_CLUB )
+                dmg:SetDamageForce( ( target:WorldSpaceCenter() - self:WorldSpaceCenter() ):GetNormalized() * 15 )
                 
-                target:EmitSound("lambdaplayers/pan/melee_frying_pan_0"..random(4)..".wav", 70)
+                target:EmitSound("lambdaplayers/pan/melee_pan_hit"..random(4)..".wav", 70)
                 
-                target:TakeDamageInfo( dmginfo )
+                target:TakeDamageInfo( dmg )
             end)
             
             return true
