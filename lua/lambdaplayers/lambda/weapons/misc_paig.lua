ec1fbf51b4f683471c5b1d482d00b73adea12c81
Floofers <thebluewulf@gmail.com>
2022-11-13 21:09:37 +0100

    revert paig to sync

--- lua/lambdaplayers/lambda/weapons/misc_paig.lua
+++ lua/lambdaplayers/lambda/weapons/misc_paig.lua
@@ -4,8 +4,7 @@ local Rand = math.Rand
 local Effect = util.Effect
 local ScreenShake = util.ScreenShake
 local BlastDamage = util.BlastDamage
-
---local convar = CreateLambdaConvar( "lambdaplayers_weapons_paigsentrybuster", 0, true, false, true, "If Lambda that spawn with the PAIG should act like the Sentry Buster. TF2 REQUIRED!", 0, 1, { type = "Bool", name = "PAIG - Enable Sentry Buster Mode", category = "Weapon Utilities" } )
+--local convar = CreateLambdaConvar( "lambdaplayers_weapons_paigsentrybuster", 0, true, false, true, "If Lambda that spawn with the PAIG have the ability to act like the Sentry Buster from TF2.", 0, 1, { type = "Bool", name = "PAIG - Enable Sentry Buster Mode", category = "Weapon Utilities" } )
 
 table.Merge( _LAMBDAPLAYERSWEAPONS, {
 
